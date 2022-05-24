958b390b045adae0b7f0a729e619ed9328bc9d9b
Harry Bryant <harrybryant16@gmail.com>
2022-05-24 09:28:57 +0100

    Ladder.tptp file added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     private static FileReader fileReader = new FileReader();
 
     public static void main(String[] args) {
-        fileReader.readFile(example1.tptp);
+        fileReader.readFile("Ladder.tptp");
     }
 }
