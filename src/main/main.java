b5124bd94cd541913a7f65f30e13738ca3efa6f4
Harry Bryant <harrybryant16@gmail.com>
2022-06-01 14:51:33 +0100

    Tseitin Transformation running from a tptp file

--- src/main/main.java
+++ src/main/main.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import tptp.Ladder;
 import tptp.LadderParser;
+import tseitin_transformation.TseitinTransformation;
 
 public class main {
     public static void main(String[] args) throws IOException {
         File f = new File("Test.tptp");
         InputStream in = new FileInputStream(f);
         Ladder l = LadderParser.parseLadder(in);
+        TseitinTransformation tt = new TseitinTransformation();
+        System.out.println(tt.transform(l));
     }
 }
