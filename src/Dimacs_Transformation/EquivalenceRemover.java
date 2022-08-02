f056bff23026231aec356668f48ca6e52a83fb65
Harry Bryant <harrybryant16@gmail.com>
2022-08-02 17:54:26 +0100

    EquivalenceRemover can deal with single element operands

--- src/Dimacs_Transformation/EquivalenceRemover.java
+++ src/Dimacs_Transformation/EquivalenceRemover.java
@@ -22,7 +22,7 @@ public class EquivalenceRemover {
     public Ladder transform(Ladder sourceL){
         Ladder targetL = new Ladder();
         for (Rung r : sourceL.getRungs()) {
-            for(Equivalence equ : splitEquivalence(r.getEquivalence().cloneWithoutConjunctions().cloneRemovingDoubleNegation())) {
+            for(Equivalence equ : splitEquivalence(r.getEquivalence())) {
                 targetL.addRung(new Rung(equ));
             }
         }
@@ -30,49 +30,42 @@ public class EquivalenceRemover {
     }
 
     private List<Equivalence> splitEquivalence(Equivalence equiv){
-        Result splitResult = splitExpression(equiv.getRhsOperand());
+        Expression splitResult = splitExpression(equiv.getRhsOperand());
 
         ArrayList<Equivalence> result = new ArrayList<>();
-        result.addAll(splitResult.equivalences);
-        result.add(new Equivalence(equiv.getLhsOperand(), splitResult.finalExpression));
+        //result.addAll(splitResult.equivalences);
+        result.add(new Equivalence(equiv.getLhsOperand(), splitResult));
 
         return result;
     }
 
-    private Result splitExpression(Expression exp){
+    private Expression splitExpression(Expression exp){
         if (exp.getClass() == Proposition.class) {
-            return new Result(exp);
+            Negation neg = new Negation(exp);
+            return neg;
         } else if (exp.getClass() == Negation.class) {
-            Negation neg = (Negation) exp;
-            Result splitResult = splitExpression(neg.getOperand());
-
-            ArrayList<Equivalence> resultEquivs = new ArrayList<>();
-            resultEquivs.addAll(splitResult.equivalences);
-            String newName = genNewName();
-            Equivalence equiv = new Equivalence(new Proposition(newName), splitResult.finalExpression);
-            resultEquivs.add(equiv);
-
-            return new Result(resultEquivs, new Negation(new Proposition(newName)));
+            return ((Negation) exp).getOperand();
         } else if (exp.getClass() == Equivalence.class || exp.getClass() == Conjunction.class) {
             throw new IllegalStateException("How the hell did we get this");
         } else if (exp.getClass() == Disjunction.class) {
-            Disjunction dis = (Disjunction) exp;
-
-            Result splitResultLhs = splitExpression(dis.getLhsOperand());
-            String newNameLhs = genNewName();
-            Equivalence equivLhs = new Equivalence(new Proposition(newNameLhs), splitResultLhs.finalExpression);
-
-            Result splitResultRhs = splitExpression(dis.getRhsOperand());
-            String newNameRhs = genNewName();
-            Equivalence equivRhs = new Equivalence(new Proposition(newNameRhs), splitResultRhs.finalExpression);
-
-            ArrayList<Equivalence> resultEquivs = new ArrayList<>();
-            resultEquivs.addAll(splitResultLhs.equivalences);
-            resultEquivs.add(equivLhs);
-            resultEquivs.addAll(splitResultRhs.equivalences);
-            resultEquivs.add(equivRhs);
-
-            return new Result(resultEquivs, new Disjunction(new Proposition(newNameLhs), new Proposition(newNameRhs)));
+//            Disjunction dis = (Disjunction) exp;
+//
+//            Result splitResultLhs = splitExpression(dis.getLhsOperand());
+//            String newNameLhs = genNewName();
+//            Equivalence equivLhs = new Equivalence(new Proposition(newNameLhs), splitResultLhs.finalExpression);
+//
+//            Result splitResultRhs = splitExpression(dis.getRhsOperand());
+//            String newNameRhs = genNewName();
+//            Equivalence equivRhs = new Equivalence(new Proposition(newNameRhs), splitResultRhs.finalExpression);
+//
+//            ArrayList<Equivalence> resultEquivs = new ArrayList<>();
+//            resultEquivs.addAll(splitResultLhs.equivalences);
+//            resultEquivs.add(equivLhs);
+//            resultEquivs.addAll(splitResultRhs.equivalences);
+//            resultEquivs.add(equivRhs);
+//
+//            return new Result(resultEquivs, new Disjunction(new Proposition(newNameLhs), new Proposition(newNameRhs)));
+            return null;
         } else {
             throw new IllegalStateException("What is thi sub type?");
         }
---
+++ src/Dimacs_Transformation_Tests/EquivalenceRemoverTests.java
@@ -0,0 +1,308 @@
+package Dimacs_Transformation_Tests;
+
+import static org.junit.Assert.assertEquals;
+
+import Dimacs_Transformation.ConjunctionRemover;
+import Dimacs_Transformation.EquivalenceRemover;
+import org.junit.Test;
+import prop_logic.Conjunction;
+import prop_logic.Disjunction;
+import prop_logic.Equivalence;
+import prop_logic.Negation;
+import prop_logic.Proposition;
+import tptp.Ladder;
+import tptp.Rung;
+
+public class EquivalenceRemoverTests {
+
+    @Test
+    public void test1(){
+        String data = "fof(ax,axiom, vA <=> vB)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("vB")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test2(){
+        String data = "fof(ax,axiom, vA <=> ~vB)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("vB")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test3(){
+        String data = "fof(ax,axiom, vA <=> vBanana_1-)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vBanana_1-"))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("vBanana_1-")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test4(){
+        String data = "fof(ax,axiom, vA <=> vB & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_4")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test5(){
+        String data = "fof(ax,axiom, vA <=> vB | vE)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_0"),new Proposition("gen_1")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test6(){
+        String data = "fof(ax,axiom, vA <=> ~vB | ~vE)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Negation(new Proposition("vB")),new Negation(new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test7(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_4")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test9(){
+        String data = "fof(ax,axiom, vA <=> ~(vB & vC))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Conjunction(new Proposition("vB"),new Proposition("vC"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test10(){
+        String data = "fof(ax,axiom, vA <=> ~(vB | vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Disjunction(new Proposition("vB"),new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Disjunction(new Proposition("gen_0"),new Proposition("gen_1")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_2")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test11(){
+        String data = "fof(ax,axiom, vA <=> ~(~vB | vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Disjunction(new Negation(new Proposition("vB")),new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_3")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test12(){
+        String data = "fof(ax,axiom, vA <=> (vB | vE) & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Disjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Disjunction(new Proposition("gen_0"),new Proposition("gen_1")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_5"),new Negation(new Proposition("gen_4")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_6"),new Disjunction(new Proposition("gen_3"),new Proposition("gen_5")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_6")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test13(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE) | vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Conjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_5"),new Negation(new Proposition("gen_4")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_6"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_5"),new Proposition("gen_6")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test14(){
+        String data = "fof(ax,axiom, vA <=> (vB | vE) | vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Disjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Disjunction(new Proposition("gen_0"),new Proposition("gen_1")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_2"),new Proposition("gen_3")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test15(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE) & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Conjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_5"),new Proposition("vC"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_6"),new Negation(new Proposition("gen_5")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_7"),new Disjunction(new Proposition("gen_4"),new Proposition("gen_6")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_7")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+
+    @Test
+    public void test16(){
+        String data = "fof(ax,axiom, vA <=> ~(vB & vE)).\n" +
+                "fof(ax,axiom, vA <=> vB & vE).";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Conjunction(new Proposition("vB"),new Proposition("vE"))))));
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_0"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_1"),new Negation(new Proposition("gen_0")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_2"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_3"),new Negation(new Proposition("gen_2")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("gen_1"),new Proposition("gen_3")))));
+
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_4"),new Proposition("vB"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_5"),new Negation(new Proposition("gen_4")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_6"),new Proposition("vE"))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_7"),new Negation(new Proposition("gen_6")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("gen_8"),new Disjunction(new Proposition("gen_5"),new Proposition("gen_7")))));
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("gen_8")))));
+
+        EquivalenceRemover er = new EquivalenceRemover();
+        assertEquals(expectedL, er.transform(sourceL));
+    }
+}
