eef502285ed05ec3a0edb881f499c75fdf743873
Harry Bryant <harrybryant16@gmail.com>
2022-08-04 14:38:17 +0100

    Cnf converter added - not testing

---
+++ src/Dimacs_Transformation/CnfConverter.java
@@ -0,0 +1,157 @@
+package Dimacs_Transformation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javafx.beans.binding.ListExpression;
+import prop_logic.Conjunction;
+import prop_logic.Disjunction;
+import prop_logic.Equivalence;
+import prop_logic.Expression;
+import prop_logic.Negation;
+import prop_logic.Proposition;
+import tptp.Ladder;
+import tptp.Rung;
+
+public class CnfConverter {
+    private static final String GEN_NAME_PREFIX = "gen_";
+    private int newVarNextNumber = 0;
+
+    private String genNewName() {
+        return GEN_NAME_PREFIX + newVarNextNumber++;
+    }
+
+    public List<Expression> transform(Ladder sourceL){
+        ArrayList<Expression> targetList = new ArrayList<>();
+        for (Rung r : sourceL.getRungs()) {
+            targetList.addAll(splitEquivalence(r.getEquivalence()));
+        }
+        return targetList;
+    }
+
+    private List<Expression> splitEquivalence(Equivalence equiv){
+        List<Expression> newExpressions = new ArrayList<>();
+
+        Expression splitResult = splitExpression(equiv.getRhsOperand());
+
+        newExpressions.addAll(generateExpressions(equiv.getLhsOperand(),
+                    splitResult));
+
+        return newExpressions;
+    }
+
+    private Expression splitExpression(Expression exp){
+        //ArrayList<Expression> expressions = new ArrayList<>();
+        if (exp.getClass() == Proposition.class) {
+            return exp;
+        } else if (exp.getClass() == Negation.class) {
+            if (((Negation) exp).getOperand().getClass() == Proposition.class) {
+                return exp;
+            } else if (((Negation) exp).getOperand().getClass() == Conjunction.class) {
+                return splitExpression(exp.cloneWithoutConjunctions().cloneRemovingDoubleNegation());
+            } else if (((Negation) exp).getOperand().getClass() == Disjunction.class) {
+                return splitExpression(exp.cloneWithoutDisjunctions().cloneRemovingDoubleNegation());
+            } else {
+                throw new IllegalStateException("How the hell did we get this");
+            }
+
+        } else if (exp.getClass() == Equivalence.class) {
+            throw new IllegalStateException("How the hell did we get this");
+        } else if (exp.getClass() == Disjunction.class) {
+            Disjunction dis = (Disjunction) exp;
+
+            if ((dis.getLhsOperand().getClass() == Conjunction.class) &&
+                    (dis.getRhsOperand().getClass() == Conjunction.class)){
+                //Do 2d loopy thing
+                return new Conjunction(new Conjunction(new Conjunction(
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getLhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getLhsOperand())),
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getLhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getRhsOperand()))),
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getRhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getLhsOperand()))),
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getRhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getRhsOperand())));
+            } else if ((dis.getLhsOperand().getClass() == Conjunction.class) &&
+                    (dis.getRhsOperand().getClass() != Conjunction.class)) {
+                //Do loopy thing
+                return new Conjunction(
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getLhsOperand()),
+                                splitExpression(dis.getRhsOperand())),
+                        new Disjunction(splitExpression(((Conjunction) dis.getLhsOperand()).getRhsOperand()),
+                                splitExpression(dis.getRhsOperand())));
+            } else if ((dis.getLhsOperand().getClass() != Conjunction.class) &&
+                    (dis.getRhsOperand().getClass() == Conjunction.class)) {
+                //Do loopy thing
+                return new Conjunction(
+                        new Disjunction(splitExpression(dis.getLhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getLhsOperand())),
+                        new Disjunction(splitExpression(dis.getLhsOperand()),
+                                splitExpression(((Conjunction) dis.getRhsOperand()).getRhsOperand())));
+            } else {
+                return new Conjunction(splitExpression(dis.getLhsOperand()),splitExpression(dis.getRhsOperand()));
+            }
+
+        } else if (exp.getClass() == Conjunction.class) {
+            Conjunction con = (Conjunction) exp;
+
+            return new Conjunction(splitExpression(con.getLhsOperand()),splitExpression(con.getRhsOperand()));
+
+        } else {
+            throw new IllegalStateException("What is this sub type?");
+        }
+    }
+
+    private List<Expression> returnWhenProposition(Expression p, Expression q){
+        List<Expression> newExpressions = new ArrayList<>();
+        Expression newExp = new Disjunction(new Negation(p), q);
+        Expression newExp2 = new Disjunction(q, new Negation(p));
+        newExpressions.add(newExp);
+        newExpressions.add(newExp2);
+
+        return newExpressions;
+    }
+
+    private List<Expression> generateExpressions(Expression p, Expression q){
+        List<Expression> newExpressions = new ArrayList<>();
+            if (q.getClass() == Proposition.class){
+                newExpressions.addAll(returnWhenProposition(p, q));
+
+            } else if (q.getClass() == Negation.class){
+                Negation neg = (Negation) q;
+                if (neg.getOperand().getClass() == Proposition.class) {
+                    newExpressions.addAll(returnWhenProposition(p, q));
+                } else {
+                    throw new IllegalStateException("How the hell did we get this");
+                }
+
+            } else if (q.getClass() == Conjunction.class){
+                Conjunction con = (Conjunction) q;
+                newExpressions.addAll(returnWhenProposition(p, con.getLhsOperand()));
+                newExpressions.addAll(returnWhenProposition(p, con.getRhsOperand()));
+
+            } else if (q.getClass() == Disjunction.class) {
+                Disjunction dis = (Disjunction) q;
+                newExpressions.addAll(returnWhenProposition(p, dis.getLhsOperand()));
+                newExpressions.addAll(returnWhenProposition(p, dis.getRhsOperand()));
+
+            }
+
+        return newExpressions;
+    }
+
+    private class Result {
+        public List<Equivalence> equivalences;
+        public Expression finalExpression;
+
+        public Result(List<Equivalence> equivalences, Expression finalExpression) {
+            this.equivalences = equivalences;
+            this.finalExpression = finalExpression;
+        }
+
+        public Result(Expression finalExpression) {
+            this.equivalences = new ArrayList<>();
+            this.finalExpression = finalExpression;
+        }
+    }
+}
---
+++ src/Dimacs_Transformation_Tests/CnfConverterTests.java
@@ -0,0 +1,244 @@
+package Dimacs_Transformation_Tests;
+
+import static org.junit.Assert.assertEquals;
+
+import Dimacs_Transformation.CnfConverter;
+import Dimacs_Transformation.CnfJavaPrinter;
+import org.junit.Test;
+import prop_logic.Conjunction;
+import prop_logic.Disjunction;
+import prop_logic.Equivalence;
+import prop_logic.Negation;
+import prop_logic.Proposition;
+import tptp.Ladder;
+import tptp.Rung;
+
+public class CnfConverterTests {
+
+    @Test
+    public void test1(){
+        String data = "fof(ax,axiom, vA <=> vB)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test2(){
+        String data = "fof(ax,axiom, vA <=> ~vB)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Proposition("vB")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test3(){
+        String data = "fof(ax,axiom, vA <=> vBanana_1-)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vBanana_1-"))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test4(){
+        String data = "fof(ax,axiom, vA <=> vB & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test5(){
+        String data = "fof(ax,axiom, vA <=> vB | vE)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test6(){
+        String data = "fof(ax,axiom, vA <=> ~vB | ~vE)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Negation(new Proposition("vB")),new Negation(new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test7(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test9(){
+        String data = "fof(ax,axiom, vA <=> ~(vB & vC))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Conjunction(new Proposition("vB"),new Proposition("vC"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test10(){
+        String data = "fof(ax,axiom, vA <=> ~(vB | vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Disjunction(new Proposition("vB"),new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test11(){
+        String data = "fof(ax,axiom, vA <=> ~(~vB | vE))";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Disjunction(new Negation(new Proposition("vB")),new Proposition("vE"))))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test12(){
+        String data = "fof(ax,axiom, vA <=> (vB | vE) & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Disjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test13(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE) | vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Conjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test14(){
+        String data = "fof(ax,axiom, vA <=> (vB | vE) | vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Disjunction(new Disjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test15(){
+        String data = "fof(ax,axiom, vA <=> (vB & vE) & vC)";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Conjunction(new Proposition("vB"),new Proposition("vE")),new Proposition("vC")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+
+    @Test
+    public void test16(){
+        String data = "fof(ax,axiom, vA <=> ~(vB & vE)).\n" +
+                "fof(ax,axiom, vA <=> vB & vE).";
+
+        Ladder sourceL = new Ladder();
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Negation(new Conjunction(new Proposition("vB"),new Proposition("vE"))))));
+        sourceL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Conjunction(new Proposition("vB"),new Proposition("vE")))));
+
+        // TODO: Calculate real expected result
+        Ladder expectedL = new Ladder();
+        expectedL.addRung(new Rung(new Equivalence(new Proposition("vA"),new Proposition("vB"))));
+
+        CnfConverter cc = new CnfConverter();
+        assertEquals(expectedL, cc.transform(sourceL));
+    }
+}
